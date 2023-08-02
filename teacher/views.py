1ef667e420dc1e11f8123d52e62623160d2641e8
Farhod <shermuhammadfarhod@gmail.com>
2023-08-02 12:15:35 +0500

    adding views

--- teacher/views.py
+++ teacher/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
-
+from .models import Teacher
+from django.http import JsonResponse
 # Create your views here.
+
+def get_all(request):
+    if request.method=="GET":
+        all_date= Teacher.objects.all()
+        result=[]
+        for teacher in all_date:
+             result.append({"id":teacher.id,"teacher_name":teacher.first_name})
+        return JsonResponse({"date":result})
+
+def get_by_id(request,student_id):
+    if request.method=="GET":
+        try:
+            date=Teacher.objects.get(id=student_id)
+        except Teacher.DoesNotExists:
+            return JsonResponse({"msg": "NOT FOUND"})
+        return JsonResponse({"id":date.id,"teacher_name":date.first_name})
+
+
+
+
